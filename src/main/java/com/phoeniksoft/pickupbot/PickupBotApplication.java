6ed0145f51cfe31d875f556d472bfeaac9a693e4
Andriy Kuzmin <andriy.kuzmin@scalors.com>
2018-10-23 12:05:20 +0300

    Implement basic main and test classes

---
+++ src/main/java/com/phoeniksoft/pickupbot/PickupBotApplication.java
@@ -0,0 +1,12 @@
+package com.phoeniksoft.pickupbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickupBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickupBotApplication.class, args);
+    }
+}
