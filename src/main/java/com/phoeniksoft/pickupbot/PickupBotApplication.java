2e7a206c448da81b0af296b1211a2787113288bf
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-11-08 23:17:23 +0100

    tested new neo4j advice store

--- src/main/java/com/phoeniksoft/pickupbot/PickupBotApplication.java
+++ src/main/java/com/phoeniksoft/pickupbot/PickupBotApplication.java
@@ -1,5 +1,8 @@
 package com.phoeniksoft.pickupbot;
 
+import com.phoeniksoft.pickupbot.domain.advisor.Advice;
+import com.phoeniksoft.pickupbot.domain.advisor.AdviceStore;
+import com.phoeniksoft.pickupbot.domain.advisor.NextAdviceParams;
 import com.phoeniksoft.pickupbot.infrastructure.neo4j.AdviceDto;
 import com.phoeniksoft.pickupbot.infrastructure.neo4j.Neo4jAdviceRepository;
 import org.springframework.boot.SpringApplication;
@@ -14,13 +17,21 @@ public class PickupBotApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(PickupBotApplication.class, args);
-        Neo4jAdviceRepository neo4JAdviceRepository = app.getBean(Neo4jAdviceRepository.class);
+        AdviceStore adviceStore = app.getBean(AdviceStore.class);
 
-        Optional<AdviceDto> cameras = neo4JAdviceRepository.getNextAdviceByUserAnswer(3L, "NO");
-        neo4JAdviceRepository.getAllStartNodes().forEach(System.out::println);
-        System.out.println(cameras.get());
-        AdviceDto defaultNode = neo4JAdviceRepository.getDefaultNode();
-        System.out.println(defaultNode);
+        Optional<Advice> advice = adviceStore.getById("3");
+        System.out.println(advice.get());
+
+        NextAdviceParams params = new NextAdviceParams();
+        params.put(NextAdviceParams.USER_ANSWER_PARAM, "YES");
+        Optional<Advice> nextAdvice = adviceStore.getNextAdvice(advice.get().getId(), params);
+        System.out.println(nextAdvice.get());
+
+        Optional<Advice> startAdvice = adviceStore.getStartAdvice();
+        System.out.println(startAdvice.get());
+
+        Advice defaultAdvice = adviceStore.getDefaultAdvice();
+        System.out.println(defaultAdvice);
 
         SpringApplication.exit(app);
     }
