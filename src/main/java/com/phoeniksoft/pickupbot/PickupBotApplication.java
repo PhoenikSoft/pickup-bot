fa4a70fe3803b310dba0aeaa4987349be6292a82
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2018-11-18 11:50:58 +0100

    fixing build

--- src/main/java/com/phoeniksoft/pickupbot/PickupBotApplication.java
+++ src/main/java/com/phoeniksoft/pickupbot/PickupBotApplication.java
@@ -15,16 +15,18 @@ public class PickupBotApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext app = SpringApplication.run(PickupBotApplication.class, args);
-        ContextFiller contextFiller = app.getBean(ContextFiller.class);
+        try {
+            ContextFiller contextFiller = app.getBean(ContextFiller.class);
 
-        UserQuery userQuery = UserQuery.builder()
-                .command(UserCommand.GET_START_ADVICE)
-                .message(new UserMessage("NO"))
-                .build();
-        userQuery.getSpecificParams().put(UserQueryParams.USER_ID_PARAM, "1123e394-a38f-4baf-9d34-23a12e39cacc");
-        UserContext userContext = contextFiller.fillContext(userQuery);
-        System.out.println(userContext);
-
-        SpringApplication.exit(app);
+            UserQuery userQuery = UserQuery.builder()
+                    .command(UserCommand.GET_START_ADVICE)
+                    .message(new UserMessage("NO"))
+                    .build();
+            userQuery.getSpecificParams().put(UserQueryParams.USER_ID_PARAM, "1123e394-a38f-4baf-9d34-23a12e39cacc");
+            UserContext userContext = contextFiller.fillContext(userQuery);
+            System.out.println(userContext);
+        } finally {
+            SpringApplication.exit(app);
+        }
     }
 }
