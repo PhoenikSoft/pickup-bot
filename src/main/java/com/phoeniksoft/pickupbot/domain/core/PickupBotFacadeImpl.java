1c0e9d29f81413b43bbc59f7960a050ba26bf72d
Yevhen Karychkovskyi <jack.karichkovskiy@gmail.com>
2019-01-26 14:36:44 +0100

    Fix and add tests

--- src/main/java/com/phoeniksoft/pickupbot/domain/core/PickupBotFacadeImpl.java
+++ src/main/java/com/phoeniksoft/pickupbot/domain/core/PickupBotFacadeImpl.java
@@ -7,14 +7,27 @@ import com.phoeniksoft.pickupbot.domain.context.UserContext;
 import com.phoeniksoft.pickupbot.domain.history.HistoryService;
 import com.phoeniksoft.pickupbot.domain.history.UserAnswersService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@AllArgsConstructor
+import javax.inject.Inject;
+
 public class PickupBotFacadeImpl implements PickupBotApi {
 
     private final ContextFiller contextFiller;
     private final Advisor advisor;
     private final HistoryService historyService;
     private final UserAnswersService userAnswersService;
+
+    @Autowired
+    public PickupBotFacadeImpl(ContextFiller contextFiller,
+                               Advisor advisor,
+                               HistoryService historyService,
+                               UserAnswersService userAnswersService) {
+        this.contextFiller = contextFiller;
+        this.advisor = advisor;
+        this.historyService = historyService;
+        this.userAnswersService = userAnswersService;
+    }
 
     @Override
     public UserAdvice getAdvice(UserQuery userQuery) {
